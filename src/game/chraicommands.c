a21b4825b80ef28657dbddad04dcef637d02c80b
Catherine Reprobate <ginger.battlequeen@gmail.com>
2024-06-13 12:07:51 -0700

    cycle aiIfChrHasWeaponEquipped

--- src/game/chraicommands.c
+++ src/game/chraicommands.c
@@ -2133,7 +2133,22 @@ bool aiIfChrHasWeaponEquipped(void)
 	struct chrdata *chr = chrFindById(g_Vars.chrdata, cmd[2]);
 	bool passes = false;
 
-	if (chr && chr->prop && chr->prop->type == PROPTYPE_PLAYER) {
+	if (isChrIdMpHumanHench(cmd[2])) {
+		struct player** playerpool = getPlayerPool(cmd[2]);
+
+		for (s32 i = 0; i < MAX_PLAYERS; i++) {
+			if (playerpool[i] && playerpool[i]->prop) {
+				u32 prevplayernum = g_Vars.currentplayernum;
+				setCurrentPlayerNum(i);
+
+				if (bgunGetWeaponNum(HAND_RIGHT) == cmd[3]) {
+					passes = true;
+				}
+
+				setCurrentPlayerNum(prevplayernum);
+			}
+		}
+	} else if (chr && chr->prop && chr->prop->type == PROPTYPE_PLAYER) {
 		u32 prevplayernum = g_Vars.currentplayernum;
 		u32 playernum = playermgrGetPlayerNumByProp(chr->prop);
 		setCurrentPlayerNum(playernum);
