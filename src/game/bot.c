6d2aba503232f43304667b0b9fead0dc34467b3f
Meowsy <155788132+Meowsy64@users.noreply.github.com>
2025-02-25 14:56:22 -0500

    Guard against underflow.

--- src/game/bot.c
+++ src/game/bot.c
@@ -816,6 +816,10 @@ bool botIsAboutToAttack(struct chrdata *chr, bool arg1)
 		mpindex = mpPlayerGetIndex(target->chr);
 		result = false;
 
+		if (chr->aibot->chrrooms[mpindex] == -1) {
+			return false;
+		}
+
 		if (chr->aibot->chrsinsight[mpindex]) {
 			result = true;
 		}
