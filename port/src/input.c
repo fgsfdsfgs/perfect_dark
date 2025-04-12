d18fb1f46b40812922cc7a04c0c64fe139073479
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-04-11 22:29:53 -0400

    added missing SDL_Version requirement on GyroThreshold.
    this attempts to solve build problem for i686-linux folks

--- port/src/input.c
+++ port/src/input.c
@@ -1954,8 +1954,13 @@ void applyGyroThreshold(f32* deltaX, f32* deltaY, f32* deltaZ, f32 threshold)
 		bool isNintendoController = false;
 		if (pads[0]) {
 				SDL_GameControllerType controllerType = SDL_GameControllerGetType(pads[0]);
+
+#if SDL_VERSION_ATLEAST(2, 0, 14)
 				isNintendoController = (controllerType == SDL_CONTROLLER_TYPE_NINTENDO_SWITCH_PRO ||
 						controllerType == SDL_CONTROLLER_TYPE_NINTENDO_SWITCH_JOYCON_PAIR);
+#else
+				isNintendoController = (controllerType == SDL_CONTROLLER_TYPE_NINTENDO_SWITCH_PRO);
+#endif
 		}
 
 		// Define dedicated deadzones per controller type
