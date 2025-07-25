6fd0d876e9f0984f18f9fabe02829c9f9276f0a4
joshuarwood <joshua.randall.wood@gmail.com>
2025-07-24 22:44:27 -0500

    updating videoCopyFramebuffer to account for depth arg

--- port/src/video.c
+++ port/src/video.c
@@ -502,7 +502,7 @@ void videoResizeFramebuffer(s32 target, u32 w, u32 h, s32 upscale, s32 autoresiz
 void videoCopyFramebuffer(s32 dst, s32 src, s32 left, s32 top)
 {
 	// assume immediate copies always read the front buffer
-	gfx_copy_framebuffer(dst, src, left, top, false);
+	gfx_copy_framebuffer(dst, src, left, top, false, false);
 }
 
 void videoResetTextureCache(void)
