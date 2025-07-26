75cb1bb44d4262f2baf3d4f1ebcc3b913465f828
joshuarwood <joshua.randall.wood@gmail.com>
2025-07-26 16:36:51 -0500

    use video methods to rescale screenx/y from native to pc resolution

--- src/game/sky.c
+++ src/game/sky.c
@@ -2519,8 +2519,8 @@ void skyCreateSunArtifact(struct artifact *artifact, s32 x, s32 y)
 		sunpos.y = env->suns[i].pos[1];
 		sunpos.z = env->suns[i].pos[2];
 		artifact->visiblelos = artifactTestLos(&sunpos, &zero, x, y) * 0xfffc;
-		artifact->screenx = (f32)x * videoGetHeight() / viewheight;
-		artifact->screeny = (viewwidth - (f32)y) * videoGetWidth() / viewwidth;
+		artifact->screenx = (f32)x / videoGetNativeWidth() * videoGetWidth();
+		artifact->screeny = (1.0f - (f32)y / videoGetNativeHeight()) * videoGetHeight();
 		artifact->zbufptr = &g_ZbufPtr1[videoGetWidth() * artifact->screeny + artifact->screenx];
 #else
 		artifact->zbufptr = &g_ZbufPtr1[(s32)camGetScreenWidth() * y + x];
