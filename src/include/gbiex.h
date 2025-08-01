7d74fe55ae7f66da4fd2dcb6a4a8eeff4efc6e9f
joshuarwood <joshua.randall.wood@gmail.com>
2025-07-31 21:49:22 -0500

    change g0 back to g since we were able to fit within a single packet

--- src/include/gbiex.h
+++ src/include/gbiex.h
@@ -223,12 +223,12 @@
 
 #define gDPCopyFramebufferEXT(pkt, dst, src, uls, ult, back, depth)   \
 {                                                                     \
-    Gfx *_g0 = (Gfx *)(pkt);                                          \
+    Gfx *_g = (Gfx *)(pkt);                                           \
                                                                       \
-    _g0->words.w0 = _SHIFTL(G_COPYFB_EXT, 24, 8)                      \
+    _g->words.w0 = _SHIFTL(G_COPYFB_EXT, 24, 8)                       \
         | _SHIFTL(dst, 11, 11) | _SHIFTL(src, 0, 11) |                \
           _SHIFTL(back, 22, 1) | _SHIFTL(depth, 23, 1);               \
-    _g0->words.w1 = _SHIFTL(uls, 16, 16) | _SHIFTL(ult, 0, 16);       \
+    _g->words.w1 = _SHIFTL(uls, 16, 16) | _SHIFTL(ult, 0, 16);        \
 }
 
 #define gDPInvalTexCacheEXT(pkt, addr)                 \
