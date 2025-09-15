436a7c66c6c140447ccd85b1c5f8e299b94ad37c
joshuarwood <joshua.randall.wood@gmail.com>
2025-09-14 20:22:41 -0500

    flush pending graphics calls before saving depth buffer to ensure it's up-to-date

--- src/game/zbuf.c
+++ src/game/zbuf.c
@@ -267,6 +267,8 @@ Gfx *zbufSaveArtifactDepths(Gfx *gdl)
 	// The saved depth is compared against on-screen depth inside
 	// schedUpdatePendingArtifacts() once the render pipeline completes.
 	g_SchedSpecialArtifactIndexes[g_SchedWriteArtifactsIndex] = 1;
+        // Flush pending draw calls to ensure the depth buffer is up-to-date
+	gDPFlushEXT(gdl++);
 	// Copy the current depth buffer to a framebuffer for safe keeping
         gDPCopyFramebufferEXT(gdl++, g_SavedDepthFb, 0, 0, 0, 0, 1);
 #endif
